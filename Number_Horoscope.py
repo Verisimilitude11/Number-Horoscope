c9c505b1fc70468dfbee00ebc1131a586da4a8af
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:43:35 -0800

    Update Number_Horoscope.py

--- Number_Horoscope.py
+++ Number_Horoscope.py
@@ -1,61 +1,40 @@
 first_name = int(input("How many letters are in your first name? "))
-
 last_name = int(input("How many letters are in your last name?"))
-
 first_name = int(input("How many letters are in your first name? "))
-
 last_name = int(input("How many letters are in your last name? "))
 
 month = int(input("What month were you born in? (January = 1) "))
-
 day = int(input("What day were you born in? "))
 
 print("")
-
 print("Your fortune:")
 
 sum1 = first_name + last_name
-
 sum2 = month + day
-
 love = sum1 * sum2
 
 print("It will be " + str(love) + " days until you fall in love.")
 
 total = 0
-
 total += first_name
-
 total += last_name
-
 total += month
-
 total += day
-
 total2 = month + day
-
 event = total % total2
 
 print("In " + str(event) + " days, an important event will happen in your life.")
 
 unlucky = month - first_name
-
 total = 0
-
 total += first_name
-
 total += last_name
-
 total += month
-
 total += day
-
 total2 = month + day
-
 event = total % total2
 
 print("In " + str(event) + " days, an important event will happen in your life.")
 
 unlucky = month - first_name
-
 print("Stay away from the number " + str(unlucky))
